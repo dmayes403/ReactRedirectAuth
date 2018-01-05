feb74bfa2d4191f3eeba6222d849516c81ba6d5c
dmayes403 <davidrmayes@gmail.com>
2018-01-04 22:16:05 -0700

    creating fake routes

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect
+} from 'react-router-dom';
+
+import './App.css';
+
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticated(cb) {
+        this.isAuthenticated = true
+        setTimeout(cb, 100)
+    },
+    signout(cb) {
+        this.isAuthenticated = false
+        setTimeout(cb, 100)
+    }
+}
+
+const Public = () => <h3>Public</h3>
+const Protected = () => <h3>Protected</h3>
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+                LOGIN
+            </div>
+        )
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <ul>
+                        <li><Link to='/public'>Public Page</Link></li>
+                        <li><Link to='/protected'>Protected Page</Link></li>
+                    </ul>
+
+                    <Route path='/public' component={Public} />
+                    <Route path='/login' component={Login} />
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
