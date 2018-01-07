086162401819e789447a0e78dd65bbb585914fa0
dmayes403 <davidrmayes@gmail.com>
2018-01-07 14:29:53 -0700

    working authentication reroute

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const fakeAuth = {
     isAuthenticated: false,
     authenticated(cb) {
         this.isAuthenticated = true
+        console.log(this.isAuthenticated);
         setTimeout(cb, 100)
     },
     signout(cb) {
@@ -24,10 +25,33 @@ const Public = () => <h3>Public</h3>
 const Protected = () => <h3>Protected</h3>
 
 class Login extends React.Component {
+    state = {
+        redirectToReferrer: false
+    }
+    
+    login = () => {
+        console.log('calling..');
+        fakeAuth.authenticated(() => {
+            console.log('calling again..');
+            this.setState({
+                redirectToReferrer: true
+            })
+        })
+    }
+
     render() {
+        const { redirectToReferrer } = this.state;
+
+        if (redirectToReferrer === true) {
+            return (
+                <Redirect to='/' />
+            )
+        }
+
         return (
             <div>
-                LOGIN
+                <p>You must log in to view this page</p>
+                <button onClick={this.login}>Log In</button>
             </div>
         )
     }
